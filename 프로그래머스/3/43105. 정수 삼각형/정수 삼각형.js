611811be03db2d8317b10e82f009986d69c10c1e
yerim <58348662+yeeeerim@users.noreply.github.com>
2024-04-26 19:36:49 +0900

    [level 3] Title: 정수 삼각형, Time: 5.24 ms, Memory: 41.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,8 @@
+function solution(triangle) {
+    for(let i = triangle.length-1; i > 0; i--) {
+        for(let j=0; j<i; j++) {
+            triangle[i-1][j] += Math.max(triangle[i][j], triangle[i][j+1]);
+        }
+    }
+    return triangle[0][0];
+}

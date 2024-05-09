204ac5f8cfc4cd592dd42e469b2d8cd4cd108a42
yerim <58348662+yeeeerim@users.noreply.github.com>
2024-05-09 18:10:56 +0900

    [level 2] Title: [3차] n진수 게임, Time: 8.12 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.js
@@ -1,26 +1,10 @@
 function solution(n, t, m, p) {
     let answer = '';
-    const arr = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-    
-    function convert(i) {
-        let copy_i = i;
-        let result = '';
-        
-        while(Math.floor(copy_i/n) > 0) {
-            const num = copy_i % n;
-            copy_i = Math.floor(copy_i/n);
-            result = arr[num] + result;
-        }
-        result = arr[copy_i] + result;
-        
-        return result;
-    }
+    let result = '';
     
     for(let i=0; i<t*m+p; i++) {
-        answer += convert(i);
+        answer += i.toString(n).toUpperCase();
     }
-    
-    let result = '';
     Array.from(answer).map((item, index) => {
         if(index % m === p-1) result+=item;
     })

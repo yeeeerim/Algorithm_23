978453fc3394aa62b358775ea14fe2566667dc38
yerim <58348662+yeeeerim@users.noreply.github.com>
2024-06-01 17:53:38 +0900

    [level 2] Title: 의상, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,20 +1,7 @@
-function combi(arr, i) {
-    let count = 0;
-    if(i===1) { arr.map((item) => { count += item; }) }
-    else {
-        arr.forEach((fixed, index) => {
-            const rest = arr.slice(index + 1);
-            count += fixed * combi(rest, i-1);
-        })
-    }
-    return count;
-}
-
 function solution(clothes) {
     var answer = 0;
     const hash = [];
     const arr = [];
-    let countOfOne = 0;
     
     clothes.forEach(item => {
         if(hash[item[1]]) hash[item[1]].push(item[0]);
@@ -23,15 +10,8 @@ function solution(clothes) {
     
     for(key of Object.keys(hash)) {
         const len = hash[key].length;
-        if(len === 1) countOfOne += 1;
-        arr.push(len);
+        arr.push(len+1);
     }
     
-    // 모든 옷 종류가 하나씩 있을 때 예외처리
-    if(countOfOne === clothes.length) return (2 ** countOfOne) - 1;
-    
-    for(let i = 1; i <= arr.length; i++) {
-        answer += combi(arr, i);
-    }
-    return answer;
+    return arr.reduce((a,b) => a*b) - 1;
 }

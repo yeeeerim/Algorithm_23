fcb1b55481119669adbce61d00ae214dd266ee24
yerim <58348662+yeeeerim@users.noreply.github.com>
2025-02-18 15:51:59 +0900

    [level 1] Title: 삼총사, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,16 @@
+function solution(number) {
+    let count = 0;
+    const len = number.length;
+    
+    for(let i=0; i<len-2; i++) {
+        for (let j=i+1; j<len-1; j++) {
+            for(let k=j+1; k<len; k++) {
+               if(number[i] + number[j] + number[k] === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
